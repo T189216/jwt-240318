a53b295c77ec86b06758b31a6ac9f96a5c7543c9
seoyeon <tus0926@naver.com>
2024-03-18 00:27:23 +0900

    start

---
+++ src/main/java/com/ll/jwt240318/Jwt240318Application.java
@@ -0,0 +1,13 @@
+package com.ll.jwt240318;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt240318Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt240318Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/jwt240318/Jwt240318ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jwt240318;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt240318ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
